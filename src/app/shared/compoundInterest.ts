a767eb1d2700a91af034988557e2a9c6db1330ee
chris.bridges <christopher_bridges@icloud.com>
2018-09-02 16:01:26 -0400

    notes for myself on formula

--- src/app/shared/compoundInterest.ts
+++ src/app/shared/compoundInterest.ts
@@ -2,3 +2,6 @@ function compoundInterest (startingAmount: number, contributionAmount: number, i
   return startingAmount * Math.pow((interestRate + 1), years) + 
   contributionAmount * ((Math.pow((1 + interestRate), years) - 1) / interestRate) * (1 + interestRate)
 }
+
+// assuming annual compounding, interest rate needs to be converted to a decimal accordingly
+  // and contribution amount needs to be calculated into amount contributed yearly
