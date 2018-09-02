0835814762507033e9735ae75a8c7304d90baf92
chris.bridges <christopher_bridges@icloud.com>
2018-09-02 13:49:58 -0400

    basic model implemented

--- src/app/shared/hero.ts
+++ src/app/shared/hero.ts
@@ -1,11 +1,21 @@
 export class Hero {
 
+  // constructor(
+  //   public id?: number,
+  //   public name?: string,
+  //   public power?: string,
+  //   public alterEgo?: string,
+  //   public email?: string) {
+
+  // }
+
   constructor(
-    public id?: number,
-    public name?: string,
-    public power?: string,
-    public alterEgo?: string,
-    public email?: string) {
+    public startingAmount: number,
+    public time: number,
+    public expectedReturn: number,
+    public contributionAmount: number,
+    public contributionFrequency: string[]
+  ) {
 
   }
 
