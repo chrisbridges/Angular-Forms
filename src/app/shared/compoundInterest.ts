466d0c1fd76dd889fc13c36bf6012a9f523783c3
chris.bridges <christopher_bridges@icloud.com>
2018-09-04 22:46:19 -0400

    supes important change

--- src/app/shared/compoundInterest.ts
+++ src/app/shared/compoundInterest.ts
@@ -1,7 +1,8 @@
-function compoundInterest (startingAmount: number, contributionAmount: number, interestRate: number, years: number) {
+function compoundInterest (startingAmount: number, contributionAmount: number, interestRate: number, years: number): number {
   return startingAmount * Math.pow((interestRate + 1), years) + 
   contributionAmount * ((Math.pow((1 + interestRate), years) - 1) / interestRate) * (1 + interestRate)
 }
 
 // assuming annual compounding, interest rate needs to be converted to a decimal accordingly
   // and contribution amount needs to be calculated into amount contributed yearly
+  
